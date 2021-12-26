f22002987603117eded84ad15cb76b1b48eb6708
Luis Garcia <lalejandro.1988@gmail.com>
2021-12-25 17:23:29 -0800

    Initial Commit

---
+++ DesignSwiftUI/DesignSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignSwiftUIApp.swift
+//  DesignSwiftUI
+//
+//  Created by Luis Garcia on 25/12/21.
+//
+
+import SwiftUI
+
+@main
+struct DesignSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
